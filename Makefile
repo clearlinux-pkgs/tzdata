ccdfb0ddf2ca77df1379076e61c957d18c6e598c
Patrick McCarty <patrick.mccarty@intel.com>
2020-12-29 20:29:19 +0000

    Enable `make update` for this package
    Signed-off-by: Patrick McCarty <patrick.mccarty@intel.com>

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
 PKG_NAME := tzdata
 
 include ../common/Makefile.common
+
+update:
+	./update.sh
---
+++ update.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+errexit() {
+	local ret=1
+	echo "Error: $1"
+	if [[ -n "$2" ]]; then
+		ret=2
+	fi
+	exit $ret
+}
+
+git pull --ff-only
+BASE_URL="https://data.iana.org/time-zones/releases"
+
+# Assumes no more than 26 releases in a given calendar year, which has been
+# true to date. Also assumes 4 digits for representing the base version
+# (calendar year).
+TARS=$(mktemp)
+trap "rm -f $TARS" EXIT
+curl -sSf "$BASE_URL"/ | grep -Eo 'tz(code|data)[0-9]{4}[a-z]\.tar\.gz' | sort -r > $TARS
+if ! grep -q '^tzcode' $TARS; then
+	errexit "no tzcode tarballs found"
+fi
+if ! grep -q '^tzdata' $TARS; then
+	errexit "no tzdata tarballs found"
+fi
+CODE_VER=$(sed -n '/^tzcode/{s/^tzcode\([0-9]\{4\}[a-z]\).*/\1/p;q}' $TARS)
+DATA_VER=$(sed -n '/^tzdata/{s/^tzdata\([0-9]\{4\}[a-z]\).*/\1/p;q}' $TARS)
+if [[ -n "$CODE_VER" ]] && [[ "$CODE_VER" != "$DATA_VER" ]]; then
+	errexit "code/data ver mismatch (code: \"$CODE_VER\"; data: \"$DATA_VER\")"
+fi
+
+CURRENT_VERSION="$(rpmspec --srpm -q --qf="%{VERSION}" tzdata.spec)"
+
+if [[ "${CURRENT_VERSION}" = "${CODE_VER}" ]]; then
+	errexit "already up-to-date" 2
+fi
+
+sed -e "s/##VERSION##/${CODE_VER}/g" tzdata.spec.in > tzdata.spec
+make generateupstream || errexit "failed to generate upstream" 3
+
+make bumpnogit
+git add tzdata.spec Makefile release upstream
+git commit -s -m "Update to ${CODE_VER}"
+make koji
